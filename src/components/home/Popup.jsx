1cb1fe4a3e2b45c73f59ca846c7a8285330b411c
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-08-11 14:46:58 +0530

    popup comment

--- src/components/home/Popup.jsx
+++ src/components/home/Popup.jsx
@@ -20,44 +20,46 @@ export default function Popup() {
   if (!visible) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 ">
-      <div className="relative bg-gradient-to-br from-white to-gray-100 rounded-3xl shadow-2xl p-8 max-w-md w-full mx-4 animate-fadeIn">
-        {showClose && (
-          <button
-            className="absolute top-3 right-3 text-gray-500 hover:text-red-500 transition cursor-pointer"
-            onClick={() => setVisible(false)}
-          >
-            <X size={24} />
-          </button>
-        )}
-        <div className="text-center space-y-4">
-   <div className="flex justify-center items-center h-20">
-  <div className="relative h-20 w-[150px]">
-    <Image
-      src="/images/logo2.png"
-      alt="Logo"
-      fill
-      className="object-contain"
-      sizes="(max-width: 640px) 100vw, 150px"
-    />
-  </div>
-</div>
-          <h2 className="text-xl lg:text-3xl font-bold text-gray-800">
-            Stay Updated With
-          </h2>
-          <h3 className=" text-lg lg:text-xl font-semibold text-violet-700">
-            Latest Job Openings & Career Updates
-          </h3>
-          <p className="text-sm text-gray-600">
-            Join thousands of users getting real-time job alerts!
-          </p>
-          <Link href="/register">
-            <button className=" px-6 py-2 cursor-pointer bg-violet-700 hover:bg-violet-800 text-white  font-medium rounded-full transition duration-300 shadow-md">
-              Register Now
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
+
+    <></>
+//     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 ">
+//       <div className="relative bg-gradient-to-br from-white to-gray-100 rounded-3xl shadow-2xl p-8 max-w-md w-full mx-4 animate-fadeIn">
+//         {showClose && (
+//           <button
+//             className="absolute top-3 right-3 text-gray-500 hover:text-red-500 transition cursor-pointer"
+//             onClick={() => setVisible(false)}
+//           >
+//             <X size={24} />
+//           </button>
+//         )}
+//         <div className="text-center space-y-4">
+//    <div className="flex justify-center items-center h-20">
+//   <div className="relative h-20 w-[150px]">
+//     <Image
+//       src="/images/logo2.png"
+//       alt="Logo"
+//       fill
+//       className="object-contain"
+//       sizes="(max-width: 640px) 100vw, 150px"
+//     />
+//   </div>
+// </div>
+//           <h2 className="text-xl lg:text-3xl font-bold text-gray-800">
+//             Stay Updated With
+//           </h2>
+//           <h3 className=" text-lg lg:text-xl font-semibold text-violet-700">
+//             Latest Job Openings & Career Updates
+//           </h3>
+//           <p className="text-sm text-gray-600">
+//             Join thousands of users getting real-time job alerts!
+//           </p>
+//           <Link href="/register">
+//             <button className=" px-6 py-2 cursor-pointer bg-violet-700 hover:bg-violet-800 text-white  font-medium rounded-full transition duration-300 shadow-md">
+//               Register Now
+//             </button>
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
   );
 }
--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -19,32 +19,34 @@ export default function Popup() {
   if (!visible) return null;
  
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm p-2">
-      <div className="relative w-full max-w-[95%] sm:max-w-[600px] md:max-w-3xl lg:max-w-5xl rounded-2xl overflow-hidden">
-        {showClose && (
-          <button
-            onClick={() => setVisible(false)}
-            className="absolute top-2 right-2 lg:right-25 z-10 rounded-full p-1 transition"
-          >
-            <X size={22} className="text-red-300 cursor-pointer" />
-          </button>
-        )}
-        <Link
-          href="https://trk.clickgenie.in/click?campaign_id=34900&pub_id=5743&p1=%7Byour-transaction-id%7D&source=%7Byour-sub-aff-id%7D"
-          target="_blank"
-        >
-          <img
-            src="/images/image.png"
-            alt="Popup"
-            className=" hidden md:block w-full h-auto max-h-[60vh] object-contain rounded-2xl"
-          />
-          <img
-            src="/images/image2.png"
-            alt="Popup"
-            className=" block md:hidden w-full h-auto max-h-[60vh] object-contain rounded-2xl"
-          />
-        </Link>
-      </div>
-    </div>
+
+    <></>
+    // <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm p-2">
+    //   <div className="relative w-full max-w-[95%] sm:max-w-[600px] md:max-w-3xl lg:max-w-5xl rounded-2xl overflow-hidden">
+    //     {showClose && (
+    //       <button
+    //         onClick={() => setVisible(false)}
+    //         className="absolute top-2 right-2 lg:right-25 z-10 rounded-full p-1 transition"
+    //       >
+    //         <X size={22} className="text-red-300 cursor-pointer" />
+    //       </button>
+    //     )}
+    //     <Link
+    //       href="https://trk.clickgenie.in/click?campaign_id=34900&pub_id=5743&p1=%7Byour-transaction-id%7D&source=%7Byour-sub-aff-id%7D"
+    //       target="_blank"
+    //     >
+    //       <img
+    //         src="/images/image.png"
+    //         alt="Popup"
+    //         className=" hidden md:block w-full h-auto max-h-[60vh] object-contain rounded-2xl"
+    //       />
+    //       <img
+    //         src="/images/image2.png"
+    //         alt="Popup"
+    //         className=" block md:hidden w-full h-auto max-h-[60vh] object-contain rounded-2xl"
+    //       />
+    //     </Link>
+    //   </div>
+    // </div>
   );
 }
