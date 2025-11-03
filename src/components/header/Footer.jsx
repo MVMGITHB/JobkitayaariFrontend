3f4b816cbfbcc8b59ed27c1c5c9b0b91faca7ba5
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-11-03 13:11:37 +0530

    facebook added

--- src/components/header/Footer.jsx
+++ src/components/header/Footer.jsx
@@ -115,7 +115,7 @@ export default function Footer() {
            <ul className="space-y-0 lg:space-y-2">
   <li>
     <Link
-      href="https://www.facebook.com/yourpage"
+      href="https://www.facebook.com/jobkityaari/"
       className="flex sm:justify-start justify-center text-white hover:underline"
       target="_blank"
       rel="noopener noreferrer"
@@ -123,7 +123,9 @@ export default function Footer() {
       <FaFacebook className="mr-2" /> Facebook
     </Link>
   </li>
-  <li>
+
+
+  {/* <li>
     <Link
       href="https://twitter.com/yourhandle"
       className="flex sm:justify-start justify-center text-white hover:underline"
@@ -132,10 +134,12 @@ export default function Footer() {
     >
       <FaTwitter className="mr-2" /> Twitter
     </Link>
-  </li>
+  </li> */}
+
+
   <li>
     <Link
-      href="https://www.instagram.com/jobkityaari/?next=%2F"
+      href="https://www.instagram.com/jobkityaari/"
       className="flex sm:justify-start justify-center text-white hover:underline"
       target="_blank"
       rel="noopener noreferrer"
@@ -143,7 +147,8 @@ export default function Footer() {
       <FaInstagram className="mr-2" /> Instagram
     </Link>
   </li>
-  <li>
+
+  {/* <li>
     <Link
       href="https://www.linkedin.com/company/yourcompany"
       className="flex sm:justify-start justify-center text-white hover:underline"
@@ -152,7 +157,8 @@ export default function Footer() {
     >
       <FaLinkedinIn className="mr-2" /> LinkedIn
     </Link>
-  </li>
+  </li> */}
+
 </ul>
           </div>
         </div>
