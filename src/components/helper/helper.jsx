056ce9f6f58458cb6ff1e39f9f9f3a9fb7c2fe98
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-01-05 10:33:16 +0530

    kjnhvgcfgvhbj

--- src/components/helper/helper.jsx
+++ src/components/helper/helper.jsx
@@ -1,12 +1,12 @@
 // const base_url = "https://naukaripakki.onrender.com";
 // const base_url = "http://localhost:5000"
 
-const url = process.env.NEXT_PUBLIC_JOB_KI_TYARI_BASE_URL;
+// const url = process.env.NEXT_PUBLIC_JOB_KI_TYARI_BASE_URL;
 
-console.log("url:", url);
+// console.log("url:", url);
 
 
- const base_url = url
+ const base_url = "https://api.jobkityaari.com"
 export const base_url1 = "https://user.jobkityaari.com"
 
 export default base_url;
