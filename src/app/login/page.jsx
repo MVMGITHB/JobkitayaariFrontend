d801084419ff20905271540d522a852aaa4de20b
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-01-09 12:33:27 +0530

    Login page soe updyed

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,33 @@
 import Login from "@/components/Auth/Login";
 
-export default function page() {
+export const metadata = {
+  title: "Login | Job Ki Tyaari",
+  description:
+    "Login to Job Ki Tyaari to access your account, job updates, exam preparation content, and resources.",
+
+  robots: {
+    index: false,
+    follow: false,
+  },
+
+  alternates: {
+    canonical: "https://jobkityaari.com/login",
+  },
+
+  openGraph: {
+    title: "Login | Job Ki Tyaari",
+    description:
+      "Login to Job Ki Tyaari and continue your job preparation journey.",
+    url: "https://jobkityaari.com/login",
+    siteName: "Job Ki Tyaari",
+    type: "website",
+  },
+};
+
+export default function Page() {
   return (
-    <div>
-     
-     
+    <main>
       <Login />
-    </div>
+    </main>
   );
 }
