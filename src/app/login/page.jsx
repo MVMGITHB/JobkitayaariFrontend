6188ac148857a6deae001edb982b80a5bd0a6ffe
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-01-09 12:41:00 +0530

    jhgcfgvh

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,26 +1,12 @@
 import Login from "@/components/Auth/Login";
 
 export const metadata = {
-  title: "Login | Job Ki Tyaari",
+  title: "Login – Apply for Govt & Private  Jobs",
   description:
     "Login to Job Ki Tyaari to access your account, job updates, exam preparation content, and resources.",
-
-  robots: {
-    index: false,
-    follow: false,
-  },
-
+  metadataBase: new URL("https://jobkityaari.com"),
   alternates: {
-    canonical: "https://jobkityaari.com/login",
-  },
-
-  openGraph: {
-    title: "Login | Job Ki Tyaari",
-    description:
-      "Login to Job Ki Tyaari and continue your job preparation journey.",
-    url: "https://jobkityaari.com/login",
-    siteName: "Job Ki Tyaari",
-    type: "website",
+    canonical: "./",
   },
 };
 
