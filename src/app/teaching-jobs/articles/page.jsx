f44e44143465e69fe34077f3ceae1b7d4284d3f6
Devansh Jinraniya <devanshjinraniya41@gmail.com>
2026-03-13 17:41:01 +0530

    loading updated

--- src/app/teaching-jobs/articles/page.jsx
+++ src/app/teaching-jobs/articles/page.jsx
@@ -1,41 +1,17 @@
 import { Blog } from "@/components/blog/Blog";
-import base_url from "@/components/helper/helper";
-import axios from "axios";
 
-export async function generateMetadata({ params }) {
-  const { slugName } = await params;
-
-  try {
-    const response = await axios.get(
-      `${base_url}/api/blog/getOneBlogByslug/${slugName}`
-    );
-    const post = response?.data;
-    if (!post) {
-      return {
-        title: "Post not found",
-        description: "This blog post could not be found.",
-        // robots: {
-        //   index: false,
-        //   follow: false,
-        // },
-      };
-    }
-
-    return {
-      title: "Teaching Jobs Articles & Exam Updates – Job Ki Tyaari",
-      description: "Teaching jobs articles with exam updates, eligibility details, preparation tips and recruitment news on Job Ki Tyaari.",
-      metadataBase: new URL('https://jobkityaari.com'),
+export async function generateMetadata() {
+  return {
+    title: "Teaching Jobs Articles & Exam Updates – Job Ki Tyaari",
+    description:
+      "Teaching jobs articles with exam updates, eligibility details, preparation tips and recruitment news on Job Ki Tyaari.",
+    metadataBase: new URL("https://jobkityaari.com"),
     alternates: {
-      canonical: './',
+      canonical: "./",
     },
-    };
-  } catch (error) {
-    return {
-      title: "Error loading post",
-      description: "An error occurred while fetching post data.",
-    };
-  }
+  };
 }
+
 const page = async () => {
   return (
     <div>
